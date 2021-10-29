1995154a17e6587befb6c7e32f9f0e63a303551f
LJL <853138590@qq.com>
2021-10-29 23:14:27 +0800

    添加一个echo的demo

--- client.c
+++ client.c
@@ -17,73 +17,73 @@
 
 int main()
 {
-	int		sockfd;
-	int		len;
-	struct	sockaddr_in address;
-	int		result;
-	char	ch = 'A';
-	char	send_buf[80];
-	char	recv_buf[80];
-	
-	//为客户创建一个套接字
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0)
-	{
-		printf("创建失败");
-		return -1;
-	}
-	else
-	{
-		printf("创建成功,等待连接\n");
-	}
-	sleep(1);	
-	
-	//根据服务器的情况给套接字命名
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = htonl(INADDR_ANY);
-	address.sin_port = htons(9734);
-	len = sizeof(address);
-	
-	//将客户端的套接字连接到服务器的套接字
-	result = connect(sockfd, (struct sockaddr *)&address, len);
-	if(result == -1)
-	{
-		perror("oops: client");
-		exit(1);
-	}
-	else
-	{
-		printf("连接成功\n");
-		printf("请编辑“序号”加内容\n");
-		printf("1.字符原样返回\n");
-		printf("2.字符大写返回\n");
-		printf("3.字符小写返回\n");
-	}
+    int		sockfd;
+    int		len;
+    struct	sockaddr_in address;
+    int		result;
+    char	ch = 'A';
+    char	send_buf[80];
+    char	recv_buf[80];
 
-    //输入要发送的数据 
+    //为客户创建一个套接字
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0)
+    {
+        printf("创建失败");
+        return -1;
+    }
+    else
+    {
+        printf("创建成功,等待连接\n");
+    }
+    sleep(1);
+
+    //根据服务器的情况给套接字命名
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(9734);
+    len = sizeof(address);
+
+    //将客户端的套接字连接到服务器的套接字
+    result = connect(sockfd, (struct sockaddr *)&address, len);
+    if(result == -1)
+    {
+        perror("oops: client");
+        exit(1);
+    }
+    else
+    {
+        printf("连接成功\n");
+        printf("请编辑“序号”加内容\n");
+        printf("1.字符原样返回\n");
+        printf("2.字符大写返回\n");
+        printf("3.字符小写返回\n");
+    }
+
+    //输入要发送的数据
     printf("Enter message to be sent:\n");
     gets(send_buf);
 
-    // 发送数据缓存           
+    // 发送数据缓存
     len = send(sockfd, send_buf, strlen(send_buf) + 1, 0);
-	//len = write(sockfd, send_buf, strlen(send_buf));
+    //len = write(sockfd, send_buf, strlen(send_buf));
     if (len != strlen(send_buf)+1)
     {
-		perror("send");
+        perror("send");
     }
-	printf("%d bytes sent\n", len);
+    printf("%d bytes sent\n", len);
 
-	// 接受数据缓存     
-	len = read(sockfd, recv_buf, sizeof(recv_buf));
-	printf("%d bytes received\n", len);
-	printf("返回信息:%s \n", recv_buf);
-	
-	//通讯结束
-	close(sockfd); 
-	exit(0);
+    // 接受数据缓存
+    len = read(sockfd, recv_buf, sizeof(recv_buf));
+    printf("%d bytes received\n", len);
+    printf("返回信息:%s \n", recv_buf);
+
+    //通讯结束
+    close(sockfd);
+    exit(0);
 }
-	
-	
-	
-	
-	
+
+
+
+
+
---
+++ easy_demo/readme.md
@@ -0,0 +1,27 @@
+# echo通讯demo
+
+### 1.编译生成server与client文件
+
+```
+chmod +x makefile
+make
+```
+
+### 2.在两个终端分别启动服务端与客户端
+
+terminal1
+
+```
+./server
+```
+
+terminal2
+
+```
+./client 127.0.0.1
+或者
+./client IP
+```
+
+### 3.在client中发送内容，server会有消息响应
+
